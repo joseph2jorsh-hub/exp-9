97b4960eebca531ff8a8fab0c8a09c5b5ea7c4a5
joseph2jorsh-hub <joseph2jorsh@gmail.com>
2026-03-20 08:35:29 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int division = a / b;
+
+        System.out.println("Simple Calculator using Docker 🚀");
+        System.out.println("Numbers: " + a + " and " + b);
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + difference);
+        System.out.println("Multiplication: " + product);
+        System.out.println("Division: " + division);
+    }
+}
